ba186ac561e80751b508f4197169cfe1d3d17f7e
Johnathan Asbee <johnathanasbee@gmail.com>
2016-08-26 00:01:25 -0400

    Converted to python 3 and cleaned up
    Converted to python 3 and cleaned up

--- distance calculation in 3d space.py
+++ distance calculation in 3d space.py
@@ -1,15 +1,17 @@
 def calc_distance_3d(x1, y1, z1, x2, y2, z2):
-    return abs(((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)**.5)
+    return abs(((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2) ** .5)
 
-x1 = input("Please input x1: ")
-y1 = input("Please input y1: ")
-z1 = input("Please input z1: ")
+x1 = int(input("Please input x1: "))
+y1 = int(input("Please input y1: "))
+z1 = int(input("Please input z1: "))
 
-x2 = input("Please input x2: ")
-y2 = input("Please input y2: ")
-z2 = input("Please input z2: ")
+x2 = int(input("Please input x2: "))
+y2 = int(input("Please input y2: "))
+z2 = int(input("Please input z2: "))
 
-print calc_distance_3d(x1, y1, z1, x2, y2, z2)
 
-x = raw_input('')
+x = calc_distance_3d(x1, y1, z1, x2, y2, z2)
 
+print('The distance between the two points is ' + str(x))
+
+input('')
